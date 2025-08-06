11d043b269bf261507aecbb5cbc080567a2effa1
qoralswn82 <58283656+qoralswn82@users.noreply.github.com>
2025-08-06 22:28:09 +0900

    fix : stack 2개를 가지고 큐 재구성
    - 스택의 FILO 특성을 사용하여 2개의 스택을 가지고 순서를 변경하여 FIFO 큐의 특성 구현

--- MyQueue.java
+++ MyQueue.java
@@ -1,36 +1,44 @@
-public class MyQueue {
+import java.util.Stack;
 
-    private int seek=0;
-    private List<Integer> stacks;
+class MyQueue {
+    // java Stack util 사용
+    // 입력, 출력 stack 생성
+    private Stack<Integer> stackIn;
+    private Stack<Integer> stackOut;
 
     public MyQueue() {
-        this.stacks = new ArrayList<>();
+        stackIn = new Stack<>();
+        stackOut = new Stack<>();
     }
     
     public void push(int x) {
-        this.stacks.add(x);
+        // 입력 스택 쌓기
+        stackIn.push(x);
     }
     
     public int pop() {
-        if(this.stacks.size()==0){
-            return -1;
+        // 출력 스택이 비었을 경우 
+        if (stackOut.isEmpty()) {
+            while (!stackIn.isEmpty()) {
+                // 입력 스택이 비워질 때 까지 출력 스택에 옮겨 pop 되는 순서 변경 
+                // stack 의 FILO 의 특성을 살려 입력 된 것을 다른 스택으로 옮겼을 경우 순서가 변경되는 것을 이용
+                stackOut.push(stackIn.pop());
+            }
         }
-        int element = this.stacks.get(0);
-        this.stacks.remove(0);
-        return element;
+        return stackOut.pop();
     }
     
     public int peek() {
-       if(this.stacks.size()==0){
-            return -1;
+        if (stackOut.isEmpty()) {
+            while (!stackIn.isEmpty()) {
+                stackOut.push(stackIn.pop());
+            }
         }
-        int element = this.stacks.get(0);
-        return element;
+        return stackOut.peek();
     }
     
     public boolean empty() {
-        return this.stacks.size()==0;
+        // 스택 입력과 출력이 모두 비었을 경우 empty true
+        return stackIn.isEmpty() && stackOut.isEmpty();
     }
-    
-
 }
