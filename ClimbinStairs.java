92d7149198b3919b3b70b5162931ccacaeafdfd2
qoralswn82 <58283656+qoralswn82@users.noreply.github.com>
2025-08-11 22:45:24 +0900

    fix :  ClimbinStairs.java
    공간최적화 적용
    - 모든 배열의 값을 저장하는 직관적인 방법이 아닌 필요한 2개의 필드만을 운용하여 O(1) 으로 시간 복잡도 줄이기

--- ClimbinStairs.java
+++ ClimbinStairs.java
@@ -4,15 +4,17 @@ public class ClimbingStairsDP {
         if (n <= 2) return n; // 1->1, 2->2 // n이 1 또는 2일 때는 결과가 n이므로 바로 반환
 
 
-        int[] dp = new int[n + 1];       // dp 배열 생성: dp[i]는 i번째 계단까지 오르는 방법의 수 저장
-        dp[1] = 1;                      // 1번째 계단까지 가는 방법은 1가지
-        dp[2] = 2;                      // 2번째 계단까지 가는 방법은 2가지 (1+1, 2)
-
-        for (int i = 3; i <= n; i++) {  // 3번째 계단부터 n번째 계단까지 반복
-            dp[i] = dp[i - 1] + dp[i - 2]; // 점화식: 현재 계단까지 방법 = 이전 계단 + 이전이전 계단 방법 수 합
+        int twoBack = 1;   // ways(1) : 1칸 계단을 오르는 방법의 수는 1
+        int oneBack = 2;   // ways(2) : 2칸 계단을 오르는 방법의 수는 2 ([1+1], [2])
+        int current = 0;   // 현재 계산 중인 ways(i)를 저장할 변수 초기화
+    
+        // 3번째 계단부터 n번째 계단까지 반복하면서 방법의 수 계산
+        for (int i = 3; i <= n; i++) {
+            current = oneBack + twoBack; // ways(i) = ways(i-1) + ways(i-2)
+            twoBack = oneBack;           // 이전 두 칸 전의 값 업데이트 (이전의 ways(i-1))
+            oneBack = current;           // 바로 이전 칸의 값 업데이트 (현재 계산한 ways(i))
         }
-
-        return dp[n];                   // 최종 n번째 계단까지 방법 수 반환
+        return current;  // n번째 계단까지 오르는 총 방법 수 반환
     }
 
     public static void main(String[] args) {
